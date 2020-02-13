e6656d279cd66bbe38d8beca33f9fdf0ed2edd7d
TaiJinYuan <alerk_star@outlook.com>
2020-02-13 09:40:55 +0800

    betAdded animation

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-// import { DontReadTheComments } from 'utils/ApolloSubscription';
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo';
 import styles from './style.module.css';
 import { convertTime } from 'utils/Utilities'
-import gql from 'graphql-tag';
-import { useSubscription } from '@apollo/react-hooks'
-import { Query, Subscription, useQuery } from 'react-apollo';
 
 const BET_SUBSCRIPTION = gql`
   subscription betAdded {
@@ -30,7 +28,6 @@ const BET_QUERY = gql`
   }
 `;
 
-
 function BetSubscription() {
     const { subscribeToMore, ...result } = useQuery(
         BET_QUERY
@@ -38,16 +35,23 @@ function BetSubscription() {
     return (
         <BetList
             {...result}
-            subscribeToNewComments={() =>
+            subscribeToNewBets={() =>
                 subscribeToMore({
                     document: BET_SUBSCRIPTION,
                     updateQuery: (prev, { subscriptionData }) => {
+                        // console.log(prev);
                         if (!subscriptionData.data) return prev;
                         const newFeedItem = subscriptionData.data.betAdded;
 
-                        return Object.assign({}, prev, {
-                            bets: ([newFeedItem, ...prev.bets].slice(0, 10))
+                        //return
+                        const newdata = Object.assign({}, prev, {
+                            // entry: {
+                            bets: ([newFeedItem, ...prev?.bets].slice(0, 10)),
+                            //     prev: prev
+                            // }
                         });
+                        // console.log(newdata);
+                        return newdata;
                     }
                 })
             }
@@ -55,38 +59,42 @@ function BetSubscription() {
     );
 }
 
-function BetList(props) {
-    const { data } = props;
-    useEffect(() => {
-        props.subscribeToNewComments();
-    }, [])
-    // console.log(data?.bets);
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.tblHeader}>
-                <div className={styles.tblTh}>TIME</div>
-                <div className={styles.tblTh}>BET</div>
-                <div className={styles.tblTh}>MULTIPLE</div>
-                <div className={styles.tblTh}>PROFIT</div>
-            </div>
-            <div className={styles.tblBody}>
-                {data && data.bets.map((bets, i) => {
-                    const bet = data.bets[data.bets.length - i -1];
-                    return (
-                    <div className={styles.tblTr} key={`row-${i}`}>
-                        <div className={styles.colWhite} key={`time-${i}`}>{convertTime(bet.time)}</div>
-                        <div className={styles.colWhite} key={`bet-${i}`}>{bet.bet / 1000}</div>
-                        <div className={styles.colWhite} key={`multiple-${i}`}>{bet.payout / 4}</div>
-                        <div className={bet.profit > 0 ? styles.profitPlus : styles.profitMinus} key={`profit-${i}`}>{bet.profit / 1000}</div>
-                    </div>
-                    )
-                })}
-                {!data && <div className={styles.tblTr}>Loading...</div>}
-            </div>
-        </div>
-    )
-}
+class BetList extends Component {
+    componentDidMount() {
+        this.props.subscribeToNewBets();
+    }
 
+    render() {
+        const { data } = this.props;
+        return (
+            <div className={styles.wrapper}>
+                <div className={styles.tblHeader}>
+                    <div className={styles.tblTh}>TIME</div>
+                    <div className={styles.tblTh}>BET</div>
+                    <div className={styles.tblTh}>MULTIPLE</div>
+                    <div className={styles.tblTh}>PROFIT</div>
+                </div>
+                <div className={styles.tblBody}>
+                    {
+                        data && data.bets.map((bet, i) => {
+                            return (
+                                <div className={styles.tblTr} key={`row-${bet.time}`}>
+                                    <div className={styles.colWhite} key={`time-${i}`}>{convertTime(bet.time)}</div>
+                                    <div className={styles.colWhite} key={`bet-${i}`}>{bet.bet / 1000}</div>
+                                    <div className={styles.colWhite} key={`multiple-${i}`}>{bet.payout / 4}</div>
+                                    <div className={bet.profit > 0 ? styles.profitPlus : styles.profitMinus} key={`profit-${i}`}>{bet.profit / 1000}</div>
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        !data && <div className={styles.tblTr}>Loading...</div>
+                    }
+                </div>
+            </div>
+        )
+    }
+}
 
 export default function Table(props) {
     return (
