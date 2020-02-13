200ff1b1be08765e9fa84f6214e57bf742df8d46
TaiJinYuan <alerk_star@outlook.com>
2020-02-14 01:35:11 +0800

    responsive

--- src/containers/TestPage/index.js
+++ src/containers/TestPage/index.js
@@ -9,50 +9,15 @@ import constants from 'utils/Constants';
 export default class TestPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            windowWidth: 0,
-            windowHeight: 0
-        };
-        this.updateDimensions = this.updateDimensions.bind(this);
-    }
-
-    componentDidMount() {
-        this.updateDimensions();
-        window.addEventListener('resize', this.updateDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
-    }
-
-    updateDimensions() {
-        let windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
-        let windowHeight = typeof window !== 'undefined' ? window.innerHeight : 0;
-        this.setState({ windowWidth, windowHeight });
     }
 
     render() {
         return (
             <ArtBoard>
                 <Header />
-                {
-                    ((this.state.windowWidth < constants.BREAK_XL) && (this.state.windowWidth > constants.BREAK_MD)) && (
-                        <>
-                            <Table />
-                            <Slider />
-                            <Clam />
-                        </>
-                    )
-                }
-                {
-                    (this.state.windowWidth > constants.BREAK_XL || this.state.windowWidth < constants.BREAK_MD) && (
-                        <>
-                            <Slider />
-                            <Table />
-                            <Clam />
-                        </>
-                    )
-                }
+                <Slider />
+                <Table />
+                <Clam />
             </ArtBoard>
         );
     }
